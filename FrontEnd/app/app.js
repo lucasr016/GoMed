d2823e151a0b0747b5ca795f2a4387a24f90fa20
Lucas Rodrigues <lrodriguescb@gmail.com>
2016-10-12 18:15:53 -0300

    Desenvolvimento tela principal

--- FrontEnd/app/app.js
+++ FrontEnd/app/app.js
@@ -6,4 +6,15 @@
       console.log('myApp is ready!');
     });
 
+    app.controller('LoginController',function(){
+      var teste = this;
+
+      teste.ClickMe = ClickMe;
+
+      function ClickMe(){
+          alert('TESTEE');
+      };
+
+    });
+
 })();
